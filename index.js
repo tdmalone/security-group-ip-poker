6be9df1eb4513aeead88effa3f9c58639d49809a
Tim Malone <tdmalone@gmail.com>
2018-03-14 07:19:11 +1100

    Complete bootstrap: CI config, config vars, todos

--- index.js
+++ index.js
@@ -9,9 +9,22 @@
 
 const aws = require( 'aws-sdk' );
 
+/* eslint-disable no-process-env */
+const config = {
+  securityGroupId: process.env.AWS_SECURITY_GROUP_ID,
+  ports:           process.env.AWS_SECURITY_GROUP_PORTS.split( ',' )
+};
+/* eslint-enable no-process-env */
+
 exports.handler = ( event, context, callback ) => {
 
   // TODO.
+
+  // Get user's IP address.
+  // @see https://stackoverflow.com/a/37560348/1982136
+
+  // Add inbound rules to the specified security group for each specified port.
+  // @see https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/ec2-example-security-groups.html
 
   callback();
 
