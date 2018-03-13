7569de31a6d2f52831f0269de559f57e8ae1cfa8
Tim Malone <tdmalone@gmail.com>
2018-03-13 18:41:40 +1100

    WIP: Package bootstrap

---
+++ index.js
@@ -0,0 +1,18 @@
+/**
+ * A simple Lambda function to 'poke' a hole in an AWS security group for temporary access from an
+ * authorised IP.
+ *
+ * @author Tim Malone <tdmalone@gmail.com>
+ */
+
+'use strict';
+
+const aws = require( 'aws-sdk' );
+
+exports.handler = ( event, context, callback ) => {
+
+  // TODO.
+
+  callback();
+
+}; // Exports.handler.
