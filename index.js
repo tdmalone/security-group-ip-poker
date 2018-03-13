868ffc3b1caacc7e016dee18c91e07947f42ec15
Tim Malone <tdmalone@gmail.com>
2018-03-14 09:43:28 +1100

    Add IP removal when running tests

--- index.js
+++ index.js
@@ -9,13 +9,15 @@
 
 const aws = require( 'aws-sdk' ),
       momentTimezone = require( 'moment-timezone' ),
-      lambdaProxyResponse = require( '@tdmalone/lambda-proxy-response' );
+      lambdaProxyResponse = require( '@tdmalone/lambda-proxy-response' ),
+      apiGatewaySampleRequest = require( 'tests/fixtures/api-gateway-request.json' );
 
 /* eslint-disable no-process-env */
 const config = {
   securityGroupId: process.env.AWS_SECURITY_GROUP_ID,
   ports:           process.env.AWS_SECURITY_GROUP_PORTS.split( ',' ),
-  timezone:        process.env.LOCAL_TIMEZONE
+  timezone:        process.env.LOCAL_TIMEZONE,
+  CI:              process.env.CI
 };
 /* eslint-enable no-process-env */
 
@@ -23,6 +25,7 @@ exports.handler = ( event, context, callback ) => {
 
   const ec2 = new aws.EC2({ apiVersion: '2016-11-15' }),
         ipAddress = event.requestContext.identity.sourceIp,
+        testIpAddress = apiGatewaySampleRequest.requestContext.identity.sourceIp,
         date = momentTimezone.tz( config.timezone ).format( 'YYYY-MM-DD h:mm A Z' );
 
   // Add inbound rules to the specified security group for each specified port.
@@ -49,9 +52,32 @@ exports.handler = ( event, context, callback ) => {
   });
 
   ec2.authorizeSecurityGroupIngress( params, ( error, response ) => {
-    if ( error ) return lambdaProxyResponse({ error: error }, null, callback );
-    const successMessage = 'Success: ' + ipAddress + ' has been added to the specified ports.';
-    return lambdaProxyResponse( null, response.length ? response : successMessage, callback );
-  });
 
+    if ( error ) {
+      return lambdaProxyResponse({ error: error }, null, callback );
+    }
+
+    const message = response.length ? response : {
+      message: 'Success: ' + ipAddress + ' has been added to the specified ports.'
+    };
+
+    // If we're not running tests, we're good to go!
+    if ( ipAddress !== testIpAddress ) {
+      return lambdaProxyResponse( null, message, callback );
+    }
+
+    // If running tests, we also need to remove the IP afterwards, otherwise any subsequent tests
+    // will fail due to trying to re-add the same IP again.
+    ec2.revokeSecurityGroupIngress( params, ( error, response ) => {
+
+      if ( error ) {
+        return lambdaProxyResponse({ error: error }, null, callback );
+      }
+
+      message.message = response.length ? response : message.message + ' Test address has been removed.';
+
+      return lambdaProxyResponse( null, message, callback );
+
+    }); // RevokeSecurityGroupIngress.
+  }); // AuthorizeSecurityGroupIngress.
 }; // Exports.handler.
