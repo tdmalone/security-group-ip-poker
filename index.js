8934b68a2069ff663ed1aaa918e89f472820c24a
Tim Malone <tdmalone@gmail.com>
2018-03-14 09:12:17 +1100

    Add majority of function + simple test

--- index.js
+++ index.js
@@ -7,25 +7,51 @@
 
 'use strict';
 
-const aws = require( 'aws-sdk' );
+const aws = require( 'aws-sdk' ),
+      momentTimezone = require( 'moment-timezone' ),
+      lambdaProxyResponse = require( '@tdmalone/lambda-proxy-response' );
 
 /* eslint-disable no-process-env */
 const config = {
   securityGroupId: process.env.AWS_SECURITY_GROUP_ID,
-  ports:           process.env.AWS_SECURITY_GROUP_PORTS.split( ',' )
+  ports:           process.env.AWS_SECURITY_GROUP_PORTS.split( ',' ),
+  timezone:        process.env.LOCAL_TIMEZONE
 };
 /* eslint-enable no-process-env */
 
 exports.handler = ( event, context, callback ) => {
 
-  // TODO.
-
-  // Get user's IP address.
-  // @see https://stackoverflow.com/a/37560348/1982136
+  const ec2 = new aws.EC2({ apiVersion: '2016-11-15' }),
+        ipAddress = event.requestContext.identity.sourceIp,
+        date = momentTimezone.tz( config.timezone ).format( 'YYYY-MM-DD h:mm A Z' );
 
   // Add inbound rules to the specified security group for each specified port.
   // @see https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/ec2-example-security-groups.html
+  // @see https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/EC2.html#authorizeSecurityGroupIngress-property
 
-  callback();
+  const params = {
+    GroupId:       config.securityGroupId,
+    IpPermissions: []
+  };
+
+  config.ports.forEach( ( port ) => {
+
+    params.IpPermissions.push({
+      IpProtocol: 'tcp',
+      FromPort: port,
+      ToPort: port,
+      IpRanges: [{
+        'CidrIp': ipAddress + '/32',
+        'Description': 'securityGroupIpPoker ' + date
+      }]
+    });
+
+  });
+
+  ec2.authorizeSecurityGroupIngress( params, ( error, response ) => {
+    if ( error ) return lambdaProxyResponse( { error: error }, null, callback );
+    const successMessage = 'Success: ' + ipAddress + ' has been added to the specified ports.';
+    return lambdaProxyResponse( null, response.length ? response : successMessage, callback );
+  });
 
 }; // Exports.handler.
